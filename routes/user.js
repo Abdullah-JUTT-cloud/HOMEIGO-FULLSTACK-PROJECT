24fbdcb17cfd1744c62033827f6f4f2b78d79b83
Abdullah <itxabdullah44@gmail.com>
2025-12-30 02:59:59 +0500

    fixed some errors

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 
 router.get("/signup",(req,res)=>{
@@ -34,9 +35,10 @@ router.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 });
 
-router.post("/login",passport.authenticate('local', { failureRedirect: '/login',failureFlash:true }),async(req,res)=>{
+router.post("/login",saveRedirectUrl,passport.authenticate('local', { failureRedirect: '/login',failureFlash:true }),async(req,res)=>{
     req.flash("success","Welcome back to HOMEIGO!");
-    res.redirect("/listings")
+    let redirectUrl=res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 });
 
 router.get("/logout",(req,res,next)=>{
@@ -44,9 +46,10 @@ router.get("/logout",(req,res,next)=>{
          if(err){
             return next(err);
          }
-    });
-    req.flash("success","you are logged out!");
+          req.flash("success","you are logged out!");
     res.redirect("/listings");
+    });
+   
 
    
 })
